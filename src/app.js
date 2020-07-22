80c703acc63e8f68db219c697d3566f1d47d2027
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-07-22 13:34:51 +0200

    Form validation with jQuery

--- src/app.js
+++ src/app.js
@@ -5,3 +5,38 @@
  * @abstract Manage contact form
  * @see JQuery (https://jquery.com)
  */
+$(document).ready(
+  () => {
+    console.log('App works!')
+
+    // Sets array of controls that are required
+    const requiredControls = [
+        'lastname-ctrl',
+        'email-ctrl'
+    ]
+    // Sets an event handler on form
+    $('#contact-form').on(
+      'change keyup',
+        (event) => {
+            console.log('Change was detected on form')
+            let isValid = true // Set form as valid before checking required fields
+            requiredControls.forEach((control) => {
+                const requiredControlValue = $('[name="' + control + '"]').val()
+                console.log(`Value for ${control} : ${requiredControlValue}`)
+                if (requiredControlValue == '') {
+                    isValid = false // Set form as invalid if at least one controle is dirty
+                }
+            })
+            console.log(`My form is : ${isValid ? 'Valid' : 'Invalid'}`)
+            if (isValid) {
+                // Well buddy, remove that fck disabled attribute
+                $('#send-button').removeAttr('disabled')
+            } else {
+                // Something went wrong with user... 
+                $('#send-button').attr('disabled', 'disabled')
+            }
+        }    
+    )
+  }  
+)
+
